ebdc3056e253ebbf287cc438bb21cff8847f33cf
SQZ0111 <saqibbhatti.public@gmail.com>
2026-06-15 15:14:01 +0200

    feat: initial project files

---
+++ live-code/variables-io.py
@@ -0,0 +1,28 @@
+#variables and data types
+# age = 20
+# volume = 2.5 * 3 * 2.5
+# is_boolean = True
+# is_not_boolean = False
+# default_url = "https://www.abc.de/files/"
+#urls = ["https://www.abc.de/files/","https://www.goodNotes.de/denied/"]
+# user = {
+#     name: "SQZ111",
+#     url: "https://github.com/SQZ0111",
+#     isActive: False
+# }
+# unknown_adress = None
+
+#print(type(20))
+#print(age)
+#print(type(volume))
+
+# print("***************VOR DER EINGABE*****************")
+
+#Eingabe annehmen
+# default_url = input("Gebe eine neue Url ein:\n")
+
+
+##AUSGABEN
+#Die Url der Seite ABC ist: ....
+# print(f"Die Url der Seite ABC ist: {default_url}")
+# print(f"2 + 2 = {2+2}")
---
+++ task/solutions/easy_calculator_solution.py
@@ -0,0 +1,39 @@
+#Einen Rechner mit den vier Grundrechnenoperationen schreibt
+
+#1. Nehme durch das Terminal zwei Operatoren als Eingabe und speichere diese
+    #1.1 input()....
+    #1.2 in variablen speichern
+
+first_operator = float(input("Gebe eine Zahl ein:\n"))
+second_operator = float(input("Gebe eine Zahl ein:\n"))
+
+
+#Tip input typecasting (umwandeln in anderen Datentypen)
+# user_number = float(input("Gebe eine Zahl ein:\n"))
+# user_number = user_number + 2
+# print(user_number)
+# user_number = user_number * 2
+# print(user_number)
+
+
+#2. Führe mit alle Grundrechenoperation mit den Operatoren aus
+   #2.1 Speichere das Ergebnis in jeweils einer Variablen
+result_div = first_operator / second_operator
+result_mul = first_operator * second_operator
+result_add = first_operator + second_operator
+result_sub = first_operator - second_operator
+
+
+#3. Gebe die Ergebnisse einzeln oder als Ganzes aus
+  #3.1 f-String im print (f"... {...}...")
+
+print(f"result divide: {result_div}\nresult sub: {result_sub}\nresult mul: {result_mul}\nresult div: {result_div}")
+# Variante 2
+# print(f"""
+# result divide: {result_div}
+# result sub: {result_sub}
+# result mul: {result_mul}
+# result div: {result_div}
+# """)
+#Nicht empfehlenswert
+#print("result divide:" ,result_div, "result sub: ", result_sub, "result mul: ", result_mul, "result div: ",result_div)
