242660744bf15bfd726f4b2b3c4ef33c9b98c055
Umut-83 <umut.agar@wabtec.com>
2026-06-16 15:20:23 +0200

    Eigene Änderungen, Solution added

--- task/login_advanced.py
+++ task/login_advanced.py
@@ -1,3 +1,6 @@
+import random
+from datetime import datetime
+
 # AUFGABE: Erweitere das Login-System mit einem einfachen Taschenrechner-Menü
 # 
 # 1. Erstelle 3 feste Benutzerkonten mit einzelnen Variablen:
@@ -55,21 +58,118 @@
 #
 
 ###START
-user_login_count = 0
-while True:
-    username = input("...")
-    if user_login_count == 3:
-        print("Programm beendet, zu viele Fehleingaben")
-        break
-    if username == "admin":
-        print("Willkommen Admin")
-        #hier geht es weiter mit den nächsten funktionen...
-        break
-    elif username == "worker" or username == "reader":
-        print("Willkommen user")
-        #hier geht es weiter mit den nächsten funktionen...
-        break
+
+# Variablen für Aufgabe 1
+user1_name = "admin"
+user1_pw = "1234"
+user1_role = "admin"
+
+user2_name = "worker"
+user2_pw = "5678"
+user2_role = "user"
+
+user3_name = "reader"
+user3_pw = "9012"
+user3_role = "user"
+
+# login_erfolgreich merkt sich, ob wir das Menü starten dürfen
+login_erfolgreich = False
+aktuelle_rolle = ""
+
+# Login-Abfrage (Aufgabe 2)
+username = input("Bitte Benutzernamen eingeben: ")
+
+if username == user1_name:
+    password = input("Bitte Passwort eingeben: ")
+    if password == user1_pw:
+        login_erfolgreich = True
+        aktuelle_rolle = user1_role
     else:
-        print("Falscher username")
-        user_login_count +=1
-    
+        print("Falsches Passwort! Programm wird beendet")
+        
+elif username == user2_name:
+    password = input("Bitte Passwort eingeben: ")
+    if password == user2_pw:
+        login_erfolgreich = True
+        aktuelle_rolle = user2_role
+    else:
+        print("Falsches Passwort! Programm wird beendet")
+        
+elif username == user3_name:
+    password = input("Bitte Passwort eingeben: ")
+    if password == user3_pw:
+        login_erfolgreich = True
+        aktuelle_rolle = user3_role
+    else:
+        print("Falsches Passwort! Programm wird beendet")
+        
+else:
+    print("Benutzer nicht gefunden! Programm wird beendet")
+
+# Aufgabe 5: Willkommensnachricht nach Rolle
+if login_erfolgreich == True:
+    if aktuelle_rolle == "admin":
+        print("Willkommen Admin! Du hast alle Rechte")
+    elif aktuelle_rolle == "user":
+        print("Willkommen User! Grundfunktionen verfügbar")
+
+    # Zusatz & Aufgabe 3: Das Menü in einer while-Schleife
+    while True:
+        print("\n--- MENÜ ---")
+        print("[1] Taschenrechner (Addition)")
+        print("[2] Taschenrechner (Subtraktion)")
+        print("[3] Taschenrechner (Multiplikation)")
+        print("[4] Taschenrechner (Division)")
+        print("[5] Uhrzeit anzeigen")
+        print("[6] Zufallszahl generieren")
+        print("[7] Programm beenden")
+        
+        option = input("Wähle eine Option: ")
+        
+        # Aufgabe 7: Programm beenden
+        if option == "7":
+            print("Programm beendet.")
+            break
+            
+        # Aufgabe 4: Taschenrechner (Optionen 1-4)
+        elif option == "1":
+            zahl1 = float(input("Erste Zahl: "))
+            zahl2 = float(input("Zweite Zahl: "))
+            ergebnis = zahl1 + zahl2
+            print("Ergebnis:", ergebnis)
+            
+        elif option == "2":
+            zahl1 = float(input("Erste Zahl: "))
+            zahl2 = float(input("Zweite Zahl: "))
+            ergebnis = zahl1 - zahl2
+            print("Ergebnis:", ergebnis)
+            
+        elif option == "3":
+            zahl1 = float(input("Erste Zahl: "))
+            zahl2 = float(input("Zweite Zahl: "))
+            ergebnis = zahl1 * zahl2
+            print("Ergebnis:", ergebnis)
+            
+        elif option == "4":
+            zahl1 = float(input("Erste Zahl: "))
+            zahl2 = float(input("Zweite Zahl: "))
+            # Division durch 0 prüfen
+            if zahl2 == 0:
+                print("Division durch 0 nicht erlaubt!")
+            else:
+                ergebnis = zahl1 / zahl2
+                print("Ergebnis:", ergebnis)
+                
+        # Aufgabe 7: Uhrzeit anzeigen
+        elif option == "5":
+            jetzt = datetime.now()
+            # Formatiert die Uhrzeit genau wie gefordert: 14:30:25
+            print("Aktuelle Uhrzeit:", jetzt.strftime("%H:%M:%S"))
+            
+        # Aufgabe 6: Zufallszahl generieren
+        elif option == "6":
+            zahl = random.randint(1, 100)
+            print("Deine Zufallszahl ist:", zahl)
+            
+        else:
+            print("Ungültige Option, bitte neu wählen.")
